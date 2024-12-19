c7ec7f7dd73e78e07e9b2bc545b5f4e0a2160613
danmirror <danu.andreanid@gmail.com>
2024-12-20 01:23:41 +0700

    update to postgresql

--- simpleproject/settings.py
+++ simpleproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'simpleproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'myproject',
+        'USER' : 'dnan',
+        'PASSWORD' : 'dnandev!',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
